d3cf724e02ca8e8d4b88010aae075821ea8b1cca
haline <857919293@qq.com>
2024-03-30 02:24:10 +0800

    添加代码

---
+++ admin_front/src/biz/home/widget/UserInfoTable/index.tsx
@@ -0,0 +1,99 @@
+import React, {useContext, useEffect, useMemo} from "react";
+import useStore from "@src/store";
+import Store from "../../store";
+import {App, Button, Space, Modal, Table} from "antd";
+import CreateUser from "../../components/CreateUserDialog";
+
+const CLUMNS = [{
+    title: 'id',
+    dataIndex: 'id',
+    key: 'id',
+},
+    {
+        title: '昵称',
+        dataIndex: 'username',
+        key: 'username',
+    },
+    {
+        title: '头像',
+        dataIndex: 'avatar',
+        key: 'avatar',
+    }, {
+        title: '出生',
+        dataIndex: 'born',
+        key: 'born',
+    },
+    {
+        title: '性别',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+    {
+        title: '创建时间',
+        dataIndex: 'createTime',
+        key: 'createTime',
+    }, {
+        title: '更新时间',
+        dataIndex: 'updateTime',
+        key: 'updateTime',
+    }]
+
+function UserInfoTable() {
+    const {userInfoList} = useStore<Store>();
+    // console.log(userInfoList);
+    const {message, modal, notification} = App.useApp();
+    // console.log(111, modal);
+
+    const dataSource = useMemo(() => {
+        return userInfoList?.map((item, index) => {
+            return {...item, key: index}
+        })
+    }, [userInfoList])
+    const createUser = (user: User) => {
+        const xx = modal.confirm({
+            footer: null,
+            title: '新增用户',
+            content: null,
+            closable: true,
+            icon: null
+        });
+        xx.update({
+            content: <CreateUser user={user} type={1} onClose={xx.destroy}/>,
+        });
+    }
+    const cloums = useMemo(() =>
+        CLUMNS.concat([
+            {
+                title: '操作',
+                key: 'action',
+                render: (_, record) => (
+                    <Space>
+                        <Button type="primary" onClick={() => {
+                            const xx = modal.confirm({
+                                footer: null,
+                                title: '修改用户',
+                                content: null,
+                                closable: true,
+                                icon: null
+                            });
+                            xx.update({
+                                content: <CreateUser user={record} onClose={xx.destroy}/>,
+                            });
+
+                        }}>编辑</Button>
+                        <Button type="primary" onClick={() => {
+                            createUser(record);
+                        }}>复制用户</Button>
+                    </Space>
+                ),
+            },
+        ]), [])
+
+    return <div>
+        <Button type="primary" onClick={createUser}>新增用户</Button>
+        <Table columns={cloums} dataSource={dataSource}/>
+    </div>
+}
+
+
+export default UserInfoTable
